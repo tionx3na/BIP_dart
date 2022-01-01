b5b83afa0f40d8abd7b83b94a5fa68307b6c84a8
xena <1runx3na2@gmail.com>
2022-01-01 18:18:55 +0530

    MnemonicGenerator statefulwidget class defined

---
+++ bip/lib/pages/mnemonic.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MnemonicGenerator extends StatefulWidget{
+  const MnemonicGenerator({Key? key}) :super(key: key);
+
+  @override 
+
+  State<MnemonicGenerator> createState() => _MnemonicGeneratorState();
+}
+
+class _MnemonicGeneratorState extends State<MnemonicGenerator> {
+  @override 
+
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      
+    );
+  }
+}
